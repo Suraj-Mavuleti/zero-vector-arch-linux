4426b228363d66c3c4dab306e76affc587e98580
Suraj Mavuleti <surajmavuleti@gmail.com>
2026-09-06 20:12:52 +0530

    Feat(v8): Visual Overhaul using CustomTkinter Modern UI Framework

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# ARCH LINUX NATIVE AUTO-UPDATER
 cd /home/suraj/.gemini/antigravity/scratch/zero_suite/zero-vector-arch-linux
 git pull origin main --quiet
-python3 zero_vector_gui.py
+/home/suraj/.gemini/antigravity/scratch/v8_env/bin/python3 zero_vector_gui.py
